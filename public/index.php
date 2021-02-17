0a10b3539ccb7048e5f11b664076bd2a2d7d7673
Den107 <deniskrv@gmail.com>
2021-02-17 21:42:08 +0300

    сделал первый класс

--- public/index.php
+++ public/index.php
@@ -1,1 +1,46 @@
 <?php
+class User
+{
+  protected int $id;
+  public string $nickName;
+  protected string $login;
+  protected string $password;
+  static protected string $role;
+  public string $avatar;
+
+  public function setPassword($pass)
+  {
+    $this->password = $pass;
+  }
+
+  public function getPassword()
+  {
+    return $this->password;
+  }
+
+  public function setLogin($login)
+  {
+    $this->login = $login;
+  }
+
+  public function getLogin()
+  {
+    return $this->login;
+  }
+
+  static public function setRole($role)
+  {
+    self::$role = $role;
+  }
+
+  static public function getRole()
+  {
+    return self::$role;
+  }
+}
+
+
+
+$man = new User();
+
+var_dump($man);
