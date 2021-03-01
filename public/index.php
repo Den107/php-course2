1c06a7903e98440ee0781d377d6fb50f443ad965
Den107 <deniskrv@gmail.com>
2021-03-01 13:02:03 +0300

    скачал файл урока

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
 <?php
+
+require "../services/Autoloader.php";
+
+spl_autoload_register([new Autoloader(), 'loadClass']);
+
+$product = new Product();
+var_dump($product);
+
+
+function cacheModel(ModelInterface $model)
+{
+  echo serialize($model->getAll());
+}
+
+cacheModel(new Autoloader());
